6996b7a12b672eaff3c10409cf1bd1469649500c
amanjain-ops <jainaman06060@gmail.com>
2024-08-26 19:03:31 +0530

    Initial Commit

---
+++ TesingSQL/TesingSQLApp.swift
@@ -0,0 +1,17 @@
+//
+//  TesingSQLApp.swift
+//  TesingSQL
+//
+//  Created by Aman Jain on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TesingSQLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
